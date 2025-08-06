9503ba27dd9739f184297ef0bba8173ba867b8ad
carlainha <160603557+carlainha@users.noreply.github.com>
2025-08-06 09:27:38 -0300

    Delete revisao directory

--- revisao/src/main/java/com/caroline/revisao/domain/exception/EntidadeEmUsoException.java
+++
@@ -1,7 +0,0 @@
-package com.caroline.revisao.domain.exception;
-
-public class EntidadeEmUsoException extends RuntimeException{
-    public EntidadeEmUsoException(String message){
-        super (message);
-    }
-}
--- revisao/src/main/java/com/caroline/revisao/domain/exception/EntidadeNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.caroline.revisao.domain.exception;
-
-public class EntidadeNaoEncontradaException extends RuntimeException{
-    public EntidadeNaoEncontradaException(String mensagem) {
-        super(mensagem);
-    }
-}
